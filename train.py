f6d4c756d548ee385a01c3e09bdb8cadc454afc4
cheesebuilding <micahchen004@gmail.com>
2024-07-12 17:10:46 +0800

    removed train.py

--- train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLOv10
-from roboflow import Roboflow
-
-model = YOLOv10("/home/via/weights/yolov10s.pt")
-
-#if you want to train own custom model you need to make a datasets folder and put your dataset in it
-model.train(data = "/home/via/datasets/CARLA-Object-Detection-2/data.yaml", epochs = 65, imgsz=800, plots=True)
